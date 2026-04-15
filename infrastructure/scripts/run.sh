8bc3a63dbacf2b1ff01ac9dcafd34d89f2f9987e
MikeTS <mike.tsvirkunov@gmail.com>
2026-04-15 09:07:05 +0600

    Change run script strategy

--- infrastructure/scripts/run.sh
+++ infrastructure/scripts/run.sh
@@ -1,5 +1,5 @@
-tofu -chdir=$OPENTOFU_WORKING_DIR init -upgrade
-tofu -chdir=$OPENTOFU_WORKING_DIR plan -no-color
-tofu -chdir=$OPENTOFU_WORKING_DIR taint twc_server.main-server
-tofu -chdir=$OPENTOFU_WORKING_DIR taint twc_server.main-argocd-node-server
-tofu -chdir=$OPENTOFU_WORKING_DIR apply -auto-approve -lock-timeout=360s
+tofu -chdir="$OPENTOFU_WORKING_DIR/maintf" init -upgrade
+tofu -chdir="$OPENTOFU_WORKING_DIR/maintf" plan -no-color
+tofu -chdir="$OPENTOFU_WORKING_DIR/maintf" taint twc_server.main-server
+tofu -chdir="$OPENTOFU_WORKING_DIR/maintf" taint twc_server.main-argocd-node-server
+tofu -chdir="$OPENTOFU_WORKING_DIR/maintf" apply -auto-approve -lock-timeout=360s
