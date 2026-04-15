9b083abb347328420751faf34de905042c79050b
MikeTS <mike.tsvirkunov@gmail.com>
2026-04-15 13:32:52 +0600

    upd: ssh keys path

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -45,12 +45,12 @@ output "argocd_node_ip" {
 
 resource "twc_ssh_key" "main-server-ssh-key" {
   name = "main-server-ssh-key"
-  body = file("./infrastructure/global/ssh/main-server-ssh-key.pub")
+  body = file("../../global/ssh/main-server-ssh-key.pub")
 }
 
 resource "twc_ssh_key" "argocd-node-server-ssh-key" {
   name = "argocd-node-server-ssh-key"
-  body = file("./infrastructure/global/ssh/argocd-node-server.pub")
+  body = file("../../global/ssh/argocd-node-server.pub")
 }
 
 
