f8a7312e5037cf56ad56fb5b4999aa9037eb5831
MikeTS <mike.tsvirkunov@gmail.com>
2026-04-21 19:14:29 +0600

    checkin2

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -90,8 +90,10 @@ resource "twc_server_ip" "main-server-ipv4" {
 
 output "main_server_ip" {
   value = twc_server_ip.main-server-ipv4.ip
+    depends_on  = [twc_server_ip.main-server-ipv4]
 }
 
 output "argocd_node_ip" {
   value = twc_server_ip.argocd-node-server-ipv4.ip
+  depends_on  = [twc_server_ip.argocd-node-server-ipv4]
 }
