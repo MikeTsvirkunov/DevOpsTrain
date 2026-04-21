b5b5f240b404b76b2b7e4c1f6ac778d344071657
MikeTS <mike.tsvirkunov@gmail.com>
2026-04-21 18:46:08 +0600

    checkin2

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -89,7 +89,7 @@ resource "twc_server_ip" "main-server-ipv4" {
 }
 
 output "main_server_ip" {
-  value = join("main_server_ip: ", twc_server_ip.main-server-ipv4.ip)
+  value = "main_server_ip: ${twc_server_ip.main-server-ipv4.ip}"
 }
 
 output "argocd_node_ip" {
