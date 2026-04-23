f1bde0f3afb90c6de7ec7ccd3e5492f6e9430602
MikeTS <mike.tsvirkunov@gmail.com>
2026-04-23 12:38:49 +0600

    upd: added skips

--- infrastructure/environments/dev/providers.tf
+++ infrastructure/environments/dev/providers.tf
@@ -15,8 +15,8 @@ terraform {
     # use_lockfile = true
     skip_region_validation = true
     skip_credentials_validation = true
-    # skip_metadata_api_check = true
-    # skip_requesting_account_id = true
-    # skip_s3_checksum = true
+    skip_metadata_api_check = true
+    skip_requesting_account_id = true
+    skip_s3_checksum = true
   }
 }
