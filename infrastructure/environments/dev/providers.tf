98c955240a583f9f8d855702038cfd171306e622
MikeTS <mike.tsvirkunov@gmail.com>
2026-04-23 09:45:41 +0600

    upd: fix try

--- infrastructure/environments/dev/providers.tf
+++ infrastructure/environments/dev/providers.tf
@@ -4,5 +4,19 @@ terraform {
       source = "tf.timeweb.cloud/timeweb-cloud/timeweb-cloud"
     }
   }
-  required_version = ">= 1.4.4"
+  required_version = ">= 1.5.3"
+  backend "s3" {
+    endpoint = "https://s3.twcstorage.ru"
+    key = "devops/train/terraform.tfstate"
+    region = "ru-1"
+    bucket="66ef2141-998128f1-c885-4840-8754-ed045ec10bb0"
+    # dynamodb_table = "terraform-state-lock"
+    encrypt = true
+    use_lockfile = true
+    skip_region_validation = true
+    skip_credentials_validation = true
+    # skip_metadata_api_check = true
+    # skip_requesting_account_id = true
+    # skip_s3_checksum = true
+  }
 }
