e573c24f154f8f6b66c7846a925b2ce659e03144
MikeTS <mike.tsvirkunov@gmail.com>
2026-04-23 12:48:36 +0600

    upd: added skips

--- infrastructure/environments/dev/providers.tf
+++ infrastructure/environments/dev/providers.tf
@@ -11,7 +11,8 @@ terraform {
     region = "ru-1"
     bucket="66ef2141-998128f1-c885-4840-8754-ed045ec10bb0"
     # dynamodb_table = "terraform-state-lock"
-    encrypt = true
+    # encrypt = true
+    encrypt = false
     # use_lockfile = true
     skip_region_validation = true
     skip_credentials_validation = true
