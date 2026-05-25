6ac9a01cce4aa244af6ea8924d5ca7430c39275e
MikeTS <mike.tsvirkunov@gmail.com>
2026-05-25 21:28:43 +0600

    adding ansible configs

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -17,25 +17,11 @@ resource "twc_server" "main-server" {
   local_network {
     id = twc_vpc.main-vpc.id
     ip = "192.168.0.15"
+    mode = "dnat_and_snat"
   }
-  # lifecycle {
-  #   replace_triggered_by = [terraform_data.always_replace.id]
-  # }
+
   project_id = data.twc_projects.mmm-project.id
   ssh_keys_ids = [twc_ssh_key.main-server-ssh-key.id]
-  # is_root_password_required = false
-  
-  # provisioner "file" {
-  #   source      = "scripts/install-k3s-agent.sh"
-  #   destination = "/tmp/install-k3s-agent.sh"
-  # }
-
-  # provisioner "remote-exec" {
-  #   inline = [
-  #     "chmod +x /tmp/install-k3s-agent.sh",
-  #     "sudo /tmp/install-k3s-agent.sh \"${aws_instance.k3s_server.private_ip}\" \"${var.private_key}\""
-  #   ]
-  # }
 }
 
 resource "twc_server" "argocd-node-server" {
@@ -49,23 +35,10 @@ resource "twc_server" "argocd-node-server" {
   }
   project_id = data.twc_projects.mmm-project.id
   ssh_keys_ids = [twc_ssh_key.argocd-node-server-ssh-key.id]
-  # is_root_password_required = false
+
   local_network {
     id = twc_vpc.main-vpc.id
     ip = "192.168.0.16"
+    mode = "dnat_and_snat"
   }
-  # lifecycle {
-  #   replace_triggered_by = [terraform_data.always_replace.id]
-  # }
-  # provisioner "file" {
-  #   source      = "scripts/install-k3s-server.sh"
-  #   destination = "/tmp/install-k3s-server.sh"
-  # }
-
-  # provisioner "remote-exec" {
-  #   inline = [
-  #     "chmod +x /tmp/install-k3s-server.sh",
-  #     "sudo /tmp/install-k3s-server.sh"
-  #   ]
-  # }
 }
